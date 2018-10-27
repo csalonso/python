1257d28387499a877a86fa991cbea537204e37e2
Caio Alonso <33733676+csalonso@users.noreply.github.com>
2018-10-27 15:15:52 -0300

    Add Problem 2 solution

--- eulerProject.py
+++ eulerProject.py
@@ -7,3 +7,21 @@ def euler1():
             soma = soma + i
         i += 1
     return soma
+
+#Fibonacci term
+def fib(x):
+    if x < 2:
+        return x
+    else:
+        return fib(x - 1) + fib(x - 2)
+
+#Problem 2   
+def euler2():
+    soma = 0
+    i = 1
+    while fib(i) < 4000000:
+        if fib(i) % 2 == 0:
+            soma = soma + fib(i)
+        i += 1
+    return soma
+
