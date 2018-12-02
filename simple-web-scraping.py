f9b2dc0c5b55fdb65102eae666837abe9ea5b349
Caio Alonso <33733676+csalonso@users.noreply.github.com>
2018-12-02 11:48:35 -0200

    Update simple-web-scraping.py

--- simple-web-scraping.py
+++ simple-web-scraping.py
@@ -1,40 +1,61 @@
-#imports
-from bs4 import BeautifulSoup
+# imports
+from bs4 import BeautifulSoup  # biblioteca de web scraping
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.error import URLError
-from collections import Counter
+from collections import Counter  # contador de objetos de coleção
 
 try:
-    #conectar a pagina
+    # conectar a pagina
     html = urlopen('https://agilemanifesto.org/principles.html')
-except HTTPError as e: #tratamento de HTTPError
+except HTTPError as e:  # tratamento de HTTPError
     print(e)
-except URLError as e: #tratamento de URLError
+except URLError as e:  # tratamento de URLError
     print(e)
 else:
-    #ler html retornado pela pagina
-    soup = BeautifulSoup(html.read(),'html.parser')
+    # ler html retornado pela pagina
+    soup = BeautifulSoup(html.read(), 'html.parser')
 
-    #buscar tags na pagina
+    # buscar tags na pagina
     tags = soup.findAll('p')
 
-    texto = ''
+    text = ''
 
-    #recebe o texto de cada tag e guarda em uma unica variavel
+    # receber o texto de cada tag e guarda em uma unica variavel
     for tag in tags:
-        texto = texto + (tag.getText())
+        text = text + (tag.getText())
 
-    #exibe o texto completo
-    print(texto)
+    # exibir o texto completo
+    print(text)
 
-    #conta quantas vezes uma palavra aparece no texto
-    print('"agile" occurs ' + str(texto.lower().split().count('agile')) + ' times in the text')
+    # dividir o texto
+    splitted_text = text.lower().split()
 
-    #verifica se uma palavra esta presente no texto
-    if 'software' in texto:
-        print('"software" occurs in the text')
 
-    #exibe as 5 palavars que mais aparecem no texto
-    c = Counter(texto.lower().split()).most_common(5)
-    print(c)
+    # contar quantas vezes uma palavra aparece no texto
+    def count_word_in_text(word, text):
+        c = str(text.count(word))
+        print(word + ' occurs ' + c + ' times in the text')
+
+    count_word_in_text('agile', splitted_text)
+    count_word_in_text('software', splitted_text)
+
+
+    # verificar se uma palavra esta presente no texto
+    def word_in_text(word, text):
+        if word in text:
+            print(word + ' occurs in the text')
+
+
+    word_in_text('software', splitted_text)
+    word_in_text('agile', splitted_text)
+    word_in_text('manifest', splitted_text)
+
+
+    # exibir as 5 palavars que mais aparecem no texto
+    def commom_words(text):
+        c = Counter(text).most_common(5)
+        print(c)
+
+
+    commom_words(splitted_text)
